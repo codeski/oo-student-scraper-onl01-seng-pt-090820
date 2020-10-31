6ded9dfaed07b39a406a37d75f83136a3dc3d5ad
Todd Stoneberg <todd.stoneberg@gmail.com>
2020-10-31 01:01:03 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -35,7 +35,7 @@ class Scraper
     social_media.css(".social-icon").each do |media| 
       binding.pry
       if media.attr('src') == "../assets/img/twitter-icon.png"
-        hash[:twitter] = social_media.css("a").attr('href').search("twitter")
+        hash[:twitter] = social_media.css("a").attr('href').value
       end
       if media.attr('src') == "../assets/img/linkedin-icon.png"
         hash[:linkedin] = social_media.css("a").attr('href').value
