9acb935e390fe0304a64e8694d5f5b52ab30752d
Todd Stoneberg <todd.stoneberg@gmail.com>
2020-10-31 02:05:46 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -18,6 +18,10 @@ class Student
 
   def add_student_attributes(attributes_hash)
     @twitter = attributes_hash[:twitter]
+    @twitter = attributes_hash[:twitter]
+    @twitter = attributes_hash[:twitter]
+    @twitter = attributes_hash[:twitter]
+    @twitter = attributes_hash[:twitter]
     binding.pry
   end
 
